2c531f4f484a1d0ef0ccf37c9e887e36123a9913
ire-design <irenemwikalii04@gmail.com>
2024-08-01 10:41:32 +0300

    continue

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -4,19 +4,38 @@ import TodoItem from "./TodoItem"
 const TodoList = () => {
 
     const todos = [
-        {id: 1,title: "todo one",description: "lorem ipsum lorem"},
-        {id: 1,title: "todo one",description: "lorem ipsum lorem"}, 
-        {id: 1,title: "todo one",description: "lorem ipsum lorem"}, 
-        {id: 1,title: "todo one",description: "lorem ipsum lorem"} 
+        {
+            id: 1,
+            title: "todo one",
+            description: "lorem ipsum lorem",
+            isComplete: true,
+        },
+        {
+            id: 1,
+            title: "todo one",
+            description: "lorem ipsum lorem",
+            isComplete: false,
+        }, 
+        {
+            id: 1,
+            title: "todo one",
+            description: "lorem ipsum lorem"
+        }, 
+        {
+            id: 1,
+            title: "todo one",
+            description: "lorem ipsum lorem",
+            isComplete: true,
+        } 
     ];
+
+    const todoItemsToRender  = todos.map(
+        //todoItems 
+    )
 
     return(
         <div id="todolist">
-            Todo List Container
-            <TodoItem title='This is my task 1' description='this is my description 1'/>
-            <TodoItem title='This is my task 2' description='this is my description 2'/>
-            <TodoItem title='This is my task 3' description='this is my description 3'/>
-            <TodoItem title='This is my task 4' description='this is my description 4'/>
+            <h2>Todo List Container</h2>
         </div>
     )
 }
