c08bfc7f836dc9ea20263e83662c0dc4bf6f6a24
ire-design <irenemwikalii04@gmail.com>
2024-08-01 10:53:40 +0300

    changes

--- src/TodoItem.jsx
+++ src/TodoItem.jsx
@@ -1,15 +1,13 @@
-import React from "react";
-// A child component of the TodoItem
 
-function TodoItem({title, description}){
-
-
-    return ( 
-        <div id="todoitem">
-            <p>Title: {title} </p>
-            <p>Description: {description}</p>
-        </div>
-    );
+import React from 'react';
+// this is a child component of <TodoList />
+function TodoItem({ title, description }) {
+  return (
+    <div id='todoitem'>
+      <p>Title: {title} </p>
+      <p>Description: {description} </p>
+    </div>
+  );
 }
- 
+
 export default TodoItem;
